0dbb9b78d96ce4319f912da0951c30d6da30f756
kapilspecial <kapilspecial@gmail.com>
2018-04-12 16:52:38 +0530

    Hello World
    Sample App created

---
+++ MyFirstStepNetCore/MyFirstStepNetCore/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyFirstStepNetCore.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Index";
+            ViewBag.Message = "Welcome to Azure World.";
+            return View();
+        }
+    }
+}
